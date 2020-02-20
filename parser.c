200dcb30ccb0aec24658f2cd54ad5b6f9df4acc4
asngl <asinghal1999@gmail.com>
2020-02-21 03:37:49 +0530

    Added String hashed mapping Table functionality

--- parser.c
+++ parser.c
@@ -1,7 +1,7 @@
 #define Max_Token_Size 30
 #define No_Of_Tokens 150
 #define mod 150
-//Keep No of tokens and mod same when necessary
+//Keep No of tokens and mod same when necessary, Keep them accurate as they are needed for Mapping table
 #define No_Of_Rules 50
 #define Max_Rule_Size 150
 #define No_Of_NT 50
@@ -22,6 +22,7 @@ struct MT{
 	Symbol s;
 	char str[Max_Token_Size];
 	enum type_of_symbol tag;
+	int flag; //To determine whether element is present or not, 1 if present
 };
 typedef struct MT MappingTable[No_Of_Tokens];
 //Structure for enum to String mapping
@@ -55,19 +56,47 @@ GRAMMAR grammarRules;
 
 
 struct MT SearchMappingTable(char str[]){
-	for(int i=0;i<No_Of_Tokens; i++){
-		if(strcmp(str,mapping[i].str)==0){
-			return mapping[i];
-		}
+	int c=0;
+	int curr=0;
+	while(str[curr]!='\0'){
+		c+=(int)str[curr];
 	}
-	printf("Token Not Found");
+	c=c%mod;
+	while(mappingString[c].flag!=0 && strcmp(str,mappingString[c].str)!=0){
+		c++;
+		c=c%mod;
+	}
+	if(mappingString[c].flag==0){
+		printf("%s Token Not found",str);
+		return NULL;
+	}
+	return mappingString[c];
 
 }
 //Function for searching returning the mapping of str Token
 
 int HashCodeMappingTable(){
+	for(int i=0;i<No_Of_Tokens;i++){
+		mappingString[i].flag=0;
+	}
 	
+	for(int i=0;mapping[i].flag==1;i++){
+		int c=0;
+		int curr=0;
+		while(mapping[i].str[curr]!='\0'){
+			c+=(int)mapping[i].str[curr];
+			curr++;
+		}
+		c=c%mod;
+		while(mappingString[c].flag!=0){
+			c++;
+			c=c%mod;
+		}
+		mappingString[c]=mapping[i];
+	}
+	return 0;
 }
+//Function to convert enum hashed Mapping table to String hashed Mapping table
 
 
 int ParseGrammarFile(char FileName[]){
@@ -77,6 +106,8 @@ int ParseGrammarFile(char FileName[]){
 		printf("Could not find Grammar Source File");
 		return 0;
 	}
+	
+	HashCodeMappingTable(); //Calling function to generate String hashed mapping table
 	int ruleNo = 0; // Vairable for tracking the current rule number
 	char grammarString[Max_Rule_size];
 	while(fgets(grammarString,Max_Rule_Size,fp)!= NULL){
@@ -92,6 +123,9 @@ int ParseGrammarFile(char FileName[]){
 		grammarToken[i]='\0';
 		struct MT tokenMap;
 		tokenMap=SearchMappingTable(grammarToken);
+		if(tokenMap==NULL){
+			continue;
+		}
 		grammarRules[ruleNo].sym = tokenMap.S.NT;
 		grammarRules[ruleNo].head=NULL;
 		while(1){
@@ -103,6 +137,9 @@ int ParseGrammarFile(char FileName[]){
 			}
 			grammarToken[i]='\0';
 			tokenMap=SearchMappingTable(grammarToken);
+			if(tokenMap==NULL){
+				continue;
+			}
 			RHSNODE *ptr,*newElement;
 			newElement = (RHSNODE *)malloc(sizeof(RHSNODE));
 			newElement->s=tokenMap.s;
