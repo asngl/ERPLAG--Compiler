eba459f32b00debd61bb0afd005cd361fd268e6e
The Dark Knight <pratik.kakade7@gmail.com>
2020-03-24 02:58:00 +0530

    Added few cases in astGenerator.c

--- ASTNodeDef.h
+++ ASTNodeDef.h
@@ -3,139 +3,139 @@
 #define _NODEDEFH
 #define STRING_MAX_SIZE 25
 
-enum boolean{BOOL_TRUE,BOOL_FALSE};
+enum boolean{BOOL_FALSE,BOOL_TRUE};
 enum NodeType{PROGRAM_NODE,MODULE_DECLARE_NODE,ID_NODE,MODULE_NODE,PARA_LIST_NODE,NUM_NODE,RNUM_NODE,BOOL_NODE,INPUT_NODE,OUTPUT_NODE,RANGE_NODE,ASSIGN_NODE,MODULE_REUSE_NODE,ID_LIST_NODE,DECLARE_NODE,CONDITION_NODE,CASE_NODE,FOR_NODE,WHILE_NODE,UNARY_NODE,BINARY_NODE};
 
 struct ProgramNode{
-    struct ASTNode *moduleDeclarations;
-    struct ASTNode *otherModules1;
-    struct ASTNode *driverModule;
-    struct ASTNode *otherModules2;
+	struct ASTNode *moduleDeclarations;
+	struct ASTNode *otherModules1;
+	struct ASTNode *driverModule;
+	struct ASTNode *otherModules2;
 };
 struct ModuleDeclareNode{
-    char moduleName[STRING_MAX_SIZE];
-  struct ASTNode *next;
+	char moduleName[STRING_MAX_SIZE];
+	struct ASTNode *next;
 };
 struct IdNode{
-    char varName[STRING_MAX_SIZE];
-  struct ASTNode *index;
+	char varName[STRING_MAX_SIZE];
+	struct ASTNode *index;
 };
 struct ModuleNode{
-    char moduleName[STRING_MAX_SIZE];
-  struct ASTNode *inputList;
-  struct ASTNode *next;
-  struct ASTNode *ret;
-  struct ASTNode *body;
+	char moduleName[STRING_MAX_SIZE];
+	struct ASTNode *inputList;
+	struct ASTNode *next;
+	struct ASTNode *ret;
+	struct ASTNode *body;
 };
 struct ParaListNode{
-    char name[STRING_MAX_SIZE];
-  struct ASTNode *type;
-  struct ASTNode *next;
-  struct ASTNode *index;
+	char name[STRING_MAX_SIZE];
+	struct ASTNode *type;
+	struct ASTNode *next;
+	struct ASTNode *index;
 };
 struct NumNode{
-    int num;
+	int num;		
 };
 struct RNumNode{
-    float rnum;
+	float rnum;
 };
 struct BoolNode{
-    enum boolean value;
+	enum boolean value;
 };
 struct InputNode{
-    char name[STRING_MAX_SIZE];
-    struct ASTNode *next;
+	char name[STRING_MAX_SIZE];
+	struct ASTNode *next;
 };
 struct OutputNode{
-    struct ASTNode *value;
-    struct ASTNode *next;
+	struct ASTNode *value;
+	struct ASTNode *next;
 };
 
 struct RangeNode{
-    struct ASTNode *Range1;
-    struct ASTNode *Range2;
+	struct ASTNode *Range1;
+	struct ASTNode *Range2;
 };
 
 struct AssignNode{
-  struct ASTNode *LHS;
-  struct ASTNode *index;
-  struct ASTNode *expr;
-  struct ASTNode *next;
+	struct ASTNode *LHS;
+	struct ASTNode *index;
+	struct ASTNode *expr;
+	struct ASTNode *next;
 };
 struct ModuleReuseNode{
-  struct ASTNode *optional;
-  char id[STRING_MAX_SIZE];
-  struct ASTNode *idList;
-  struct ASTNode *next;
+	struct ASTNode *optional;
+	char id[STRING_MAX_SIZE];
+	struct ASTNode *idList;
+	struct ASTNode *next;
 };
 struct IdListNode{
-  char varName[STRING_MAX_SIZE];
-  struct ASTNode *next;
+	char varName[STRING_MAX_SIZE];
+	struct ASTNode *next;
 };
 struct DeclareNode{
-  struct ASTNode *idList;
-  char dataType[STRING_MAX_SIZE];
-  struct ASTNode *Range;
-  struct ASTNode *next;
+	struct ASTNode *idList;
+	char dataType[STRING_MAX_SIZE];
+	struct ASTNode *Range;
+	struct ASTNode *next;
 };
 struct ConditionNode{
-  char id[STRING_MAX_SIZE];
-  struct ASTNode *Case;
-  struct ASTNode *Default;
-  struct ASTNode *next;
+	char id[STRING_MAX_SIZE];
+	struct ASTNode *Case;
+	struct ASTNode *Default;
+	struct ASTNode *next;
 };
 struct CaseNode{
-  struct ASTNode *value;
-  struct ASTNode *stmt;
-  struct ASTNode *next;
+	struct ASTNode *value;
+	struct ASTNode *stmt;
+	struct ASTNode *next;
 };
 struct ForNode{
-  char id[STRING_MAX_SIZE];
-  struct ASTNode *range;
-  struct ASTNode *stmt;
-  struct ASTNode *next;
+	char id[STRING_MAX_SIZE];
+	struct ASTNode *range;
+	struct ASTNode *stmt;
+	struct ASTNode *next;
 };
 struct WhileNode{
-  struct ASTNode *expr;
-  struct ASTNode *stmt;
-  struct ASTNode *next;
+	struct ASTNode *expr;
+	struct ASTNode *stmt;
+	struct ASTNode *next;
 };
 struct UnaryNode{
-  char op[STRING_MAX_SIZE];
-  struct ASTNode *expr;
+	char op[STRING_MAX_SIZE];
+	struct ASTNode *expr;
 };
 struct BinaryNode{
-  char op[STRING_MAX_SIZE];
-  struct ASTNode *expr1;
-  struct ASTNode *expr2;
+	char op[STRING_MAX_SIZE];
+	struct ASTNode *expr1;
+	struct ASTNode *expr2;
 };
 
 
 union untaggedASTNode{
-    struct ProgramNode programNode;
-    struct ModuleDeclareNode moduleDeclareNode;
-    struct IdNode idNode;
-    struct ModuleNode moduleNode;
-    struct ParaListNode paraList;
-    struct NumNode numNode;
-    struct RNumNode rNumNode;
-    struct BoolNode boolNode;
-    struct InputNode inputNode;
-    struct OutputNode outputNode;
-    struct RangeNode rangeNode;
-    struct AssignNode assignNode;
-    struct IdListNode idListNode;
-    struct DeclareNode declareNode;
-    struct ConditionNode conditionNode;
-    struct CaseNode caseNode;
-    struct ForNode forNode;
-    struct WhileNode whileNode;
-    struct UnaryNode unaryNode;
-    struct BinaryNode binaryNode;
+	struct ProgramNode programNode;
+	struct ModuleDeclareNode moduleDeclareNode;
+	struct IdNode idNode;
+	struct ModuleNode moduleNode;
+	struct ParaListNode paraList;
+	struct NumNode numNode;
+	struct RNumNode rNumNode;
+	struct BoolNode boolNode;
+	struct InputNode inputNode;
+	struct OutputNode outputNode;
+	struct RangeNode rangeNode;
+	struct AssignNode assignNode;
+	struct IdListNode idListNode;
+	struct DeclareNode declareNode;
+	struct ConditionNode conditionNode;
+	struct CaseNode caseNode;
+	struct ForNode forNode;
+	struct WhileNode whileNode;
+	struct UnaryNode unaryNode;
+	struct BinaryNode binaryNode;
 };
 struct ASTNode{
-    union untaggedASTNode node;
-    enum  NodeType tag;
+	union untaggedASTNode node;
+	enum  NodeType tag;
 };
 
 #endif
--- astGenerator.c
+++ astGenerator.c
@@ -1,5 +1,4 @@
 #include "ASTNodeDef.h"
-#include "lexerDef.h"
 #include "parserDef.h"
 #include <stdlib.h>
 #include <string.h>
@@ -21,28 +20,88 @@ struct ASTNode *createAST(struct ParseTreeNode *root)
     switch(root->ruleNumber)
     {
         case 0:
-           result=createASTNode(PROGRAM_NODE);
-           result->node.programNode.moduleDeclarations=createAST(getNthChild(root,1));
-           result->node.programNode.otherModules1=createAST(getNthChild(root,2));
-           result->node.programNode.driverModule=createAST(getNthChild(root,3));
-           result->node.programNode.otherModules2=createAST(getNthChild(root,4));
-           break;   
+		result=createASTNode(PROGRAM_NODE);
+		result->node.programNode.moduleDeclarations=createAST(getNthChild(root,1));
+		result->node.programNode.otherModules1=createAST(getNthChild(root,2));
+		result->node.programNode.driverModule=createAST(getNthChild(root,3));
+		result->node.programNode.otherModules2=createAST(getNthChild(root,4));
+		break;   
         case 1:
-            result=createAST(getNthChild(root,1));
-            result->node.moduleDeclareNode.next=createAST(getNthChild(root,2));
-            break;
+		result=createAST(getNthChild(root,1));
+		result->node.moduleDeclareNode.next=createAST(getNthChild(root,2));
+		break;
         case 2:
-            result=NULL;
-            break;
+		result=NULL;
+		break;
         case 3:
-            result=createASTNode(MODULE_DECLARE_NODE);
-            strcpy(result->node.moduleDeclareNode.moduleName,getNthChild(root,3)->token_info.lexeme);
-            //next=0.next DONE IN PARENT in case 2
-            break;
+		result=createASTNode(MODULE_DECLARE_NODE);
+		strcpy(result->node.moduleDeclareNode.moduleName,getNthChild(root,3)->token_info.lexeme);
+		//next=0.next DONE IN PARENT in case 2
+		break;
         case 4:
+		result=createAST(getNthChild(root,1));
+		result->node.moduleNode.next=createAST(getNthChild(root,2));
+		break;
+	case 5:
+		result=NULL;
+		break;
+	case 6:
+		result=createAST(getNthChild(root,5);
+		break;
+	case 7:
+		result=createASTNode(MODULE_NODE);
+		strcpy(result->node.moduleNode.moduleName, getNthChild(root,3)->token_info.lexeme);
+		result->node.moduleNode.inputList=createAST(getNthChild(root,8));
+		result->node.moduleNode.ret=createAST(getNthChild(root,11));
+		result->node.moduleNode.body=createAST(getNthChild(root,12));
+		break;
+	case 8:
+		result=createAST(getNthChild(root,3);
+		break;
+	case 9:
+		result=NULL;
+		break;
+//	case 10:
+//		result=createASTNode(PARA_LIST_NODE);
+//		strcpy(result->node.paraListNode.name, getNthChild(root,1)->token_info.lexeme);
+//		result->node.paraListNode.type=getNthChild(root,3);
+//		result->node.paraListNode.next=getNthChild(root,4);
+//		result->node.paraListNode.index=getNthChild(root,);
+//		break;
+//	cases 10-14
+	case 15:
+		result=NULL;
+		break;
+	case 16:
+		result=createASTNode(NumNode):///////////////check 16-18
+		break;
+	case 17:
+		result=createASTNode(RNumNode):
+		break;
+	case 18:
+		result=createASTNode(BoolNode):
+		break;
+        case 59:
         	result=createAST(getNthChild(root,1));
-            result->node.moduleNode.next=createAST(getNthChild(root,2));
-           	break;
+        	break;
+        case 60:    	
+        	result=createASTNode(UNARY_NODE);
+        	strcpy(result->node.op,getNthChild(root,1)->token_info.lexeme);
+        	result->node.expr=createAST(getNthChild(root,2));
+        	break;
+        //case 61:
+        //	result=createAST(getNthChild(root,2));
+        //cases 61-64
+        case 65:
+        	result=createASTNode(BOOL_NODE);
+        	result->node.value=BOOL_TRUE;
+        	break;
+        case 66:
+        	result=createASTNode(BOOL_NODE);
+        	result->node.value=BOOL_FALSE;
+        	break;
+        
+        	
     }
     return result;
 }
