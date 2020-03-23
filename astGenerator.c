830ce42436b7232a4a0ce182c0a7ecf8984175d9
YashIsDeep <yashparth.gupta@gmail.com>
2020-03-24 00:12:26 +0530

    corrected code to ASTGenerator.c

--- astGenerator.c
+++ astGenerator.c
@@ -17,26 +17,26 @@ struct ASTNode *createAST(struct ParseTreeNode *root)
     {
         case 0:
            result=createASTNode(PROGRAM_NODE);
-           result->moduleDeclarations=createAST(getNthChild(root,1));
-           result->otherModules1=createAST(getNthChild(root,2));
-           result->driverModule=createAST(getNthChild(root,3));
-           result->otherModules2=createAST(getNthChild(root,4));
+           result->node->programNode->moduleDeclarations=createAST(getNthChild(root,1));
+           result->node->programNode->otherModules1=createAST(getNthChild(root,2));
+           result->node->programNode->driverModule=createAST(getNthChild(root,3));
+           result->node->programNode->otherModules2=createAST(getNthChild(root,4));
            break;   
         case 1:
             result=createAST(getNthChild(root,1));
-            result->next=createAST(getNthChild(root,2));
+            result->node->moduleDeclareNode->next=createAST(getNthChild(root,2));
             break;
         case 2:
             result=NULL;
             break;
         case 3:
             result=createASTNode(MODULE_DECLARE_NODE);
-            strcpy(result->moduleName,getNthChild(root,3)->token_info.lexeme);
+            strcpy(result->node->moduleDeclareNode->moduleName,getNthChild(root,3)->token_info.lexeme);
             //next=0.next DONE IN PARENT in case 2
             break;
         case 4:
         	result=createAST(getNthChild(root,1));
-            result->next=createAST(getNthChild(root,2));
+            result->node->moduleNode->next=createAST(getNthChild(root,2));
            	break;
     }
     return result;
