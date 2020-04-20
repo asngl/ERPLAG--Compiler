19df6dfcbf1a4c028cefcb2940e16f438af4d970
asngl <asinghal1999@gmail.com>
2020-04-20 13:10:50 +0530

    Added comments

--- symbolTableDef.h
+++ symbolTableDef.h
@@ -11,13 +11,16 @@ Ayush Singhal  2017A7PS0116P
 #include "ASTNodeDef.h"
 #define MAX_IDENTIFIER_LENGTH 25
 #define MOD 127
-//enum Datatype{DT_INTEGER,DT_REAL,DT_BOOLEAN,DT_ARRAY};
+
 extern int ERROR_FLAG;
+
+//Union for storing bound of an array
 typedef union ArrayBound{
 	int bound;
 	char lexeme[MAX_IDENTIFIER_LENGTH+1];
 }ArrayBound;
 
+//Structure for storing Type 
 typedef struct Type
 {
 	enum Datatype type;
@@ -31,6 +34,7 @@ typedef struct Type
 	int isStatic;	//1 for Static, 0 for Dynamic
 }Type;
 
+//Structure for storing a single variable entry
 typedef struct VariableEntry
 {
 	char varName[25];
@@ -43,9 +47,10 @@ typedef struct VariableEntry
 	struct VariableEntry *next;// For collisions
 }VariableEntry;
 
-
+//Array of variable entry to implement hashed arrays
 typedef VariableEntry *VariableEntryTable[MOD];
 
+//Structure for storing parameter information
 typedef struct ParameterList
 {
 	char varName[MAX_IDENTIFIER_LENGTH+1];
@@ -58,10 +63,13 @@ typedef struct ParameterList
 	struct ParameterList *next;
 }ParameterList;
 
+//Structure for storing scope
 typedef struct Scope{
 	int startLine;
 	int endLine;
 }Scope;
+
+//Structure for storing all the variables in a given scope
 typedef struct LocalTable
 {
 	struct LocalTable *parent;
@@ -73,6 +81,7 @@ typedef struct LocalTable
 	VariableEntryTable variableTable;
 }LocalTable;
 
+//Stucture for storing all the local tables corresponding to a function
 typedef struct FunctionTable{
 	char funcName[MAX_IDENTIFIER_LENGTH+1];
 	int declareFlag;
@@ -91,17 +100,20 @@ typedef struct FunctionTable{
 	struct FunctionTable *next; // For Collisions
 }FunctionTable;
 
+//Structure to implement symbol table as hashed arrays
 typedef struct SymbolTableEntry{
 	FunctionTable *pointer;
 }SymbolTableEntry;
 
 typedef SymbolTableEntry SymbolTable[MOD];
 
+//Structure to store a string to avoid error repetition of a given undeclared variable
 typedef struct StringList{
 	char varName[MAX_IDENTIFIER_LENGTH+1];
 	struct StringList *next;
 }StringList;
 
+//Structure for passing information such as forbidden variables, etc. for type checking
 typedef struct Context{
 	SymbolTableEntry **symbolTable;
 	char funcName[MAX_IDENTIFIER_LENGTH+1];
